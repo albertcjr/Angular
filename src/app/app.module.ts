5645118609fd332a724c4a295beefe69f3895b6c
Albert C <albertcjr@outlook.com>
2018-08-02 01:15:35 +0800

    Commit 08-01
    Commit 08-01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products.component';
@@ -8,6 +9,15 @@ import { NewsComponent } from './components/news/news.component';
 import { CustomerComponent } from './components/customer/customer.component';
 import { PartsComponent } from './components/parts/parts.component';
 import { WorksComponent } from './components/works/works.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { FormsModule } from '@angular/forms';
+
+import { UserdataService } from './providers/userdata.service';
+import { MaintenanceComponent } from './components/maintenance/maintenance.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { PartsService } from './services/parts/parts.service';
+
 
 @NgModule({
   declarations: [
@@ -16,15 +26,25 @@ import { WorksComponent } from './components/works/works.component';
     NewsComponent,
     CustomerComponent,
     PartsComponent,
-    WorksComponent
+    WorksComponent,
+    AboutComponent,
+    ServicesComponent,
+    MaintenanceComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
-      {path:'News',component: NewsComponent}
+      {path:'News',component: NewsComponent},
+      {path:'about',component: AboutComponent},
+      {path:'services',component: ServicesComponent},
+      {path: 'maintenance', component: MaintenanceComponent},
+      {path: 'admin', component: AdminComponent}
     ])
   ],
-  providers: [],
+  providers: [UserdataService,PartsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
